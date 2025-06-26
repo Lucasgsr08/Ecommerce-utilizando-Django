ea2e8ca5fd40380b8143d3d04a38048e24ea2e2d
lucas <l_gabriel8@outlook.com>
2025-06-26 11:00:35 -0300

    apos essas modificaçoes, o botao de ajuda, contato e sobre estao funcionando

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,18 @@
-# store/urls.py
-
 from django.urls import path
 from . import views
 
 app_name = 'store'
 
 urlpatterns = [
+    
+        # Novas rotas
+    path('contato/', views.contato, name='contato'),
+    path('ajuda/', views.ajuda, name='ajuda'),
+    path('sobre/', views.sobre, name='sobre'),
+    
+    
     path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,5 @@
-# store/views.py
-
 from django.shortcuts import render, get_object_or_404
-from .models import Category, Product
+from .models import Product, Category
 
 def product_list(request, category_slug=None):
     category = None
@@ -10,17 +8,23 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request,
-                          'store/product/list.html',
-                          {'category': category,
-                           'categories': categories,
-                           'products': products})
+    return render(request, 'store/product/list.html', {
+        'category': category,
+        'categories': categories,
+        'products': products
+    })
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(Product,
-                                        id=id,
-                                        slug=slug,
-                                        available=True)
-    return render(request,
-                          'store/product/detail.html',
-                          {'product': product})
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, 'store/product/detail.html', {
+        'product': product
+    })
+
+def ajuda(request):
+    return render(request, 'store/ajuda.html')
+
+def sobre(request):
+    return render(request, 'store/sobre.html')
+
+def contato(request):
+    return render(request, 'store/contato.html')
