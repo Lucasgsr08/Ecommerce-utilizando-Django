48ef5527e85d270cce5739fbd8211126d4339081
lucas <l_gabriel8@outlook.com>
2025-06-27 16:31:53 -0300

    a partir desse momento todos os botoes da loja estao funcionando

--- cart/urls.py
+++ cart/urls.py
@@ -6,5 +6,6 @@ app_name = 'cart'
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
     path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+
 ]
