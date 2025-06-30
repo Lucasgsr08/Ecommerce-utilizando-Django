24fe3d1b7eb0fd7568e32d3a2dfc3a023168aee3
lucas <l_gabriel8@outlook.com>
2025-06-30 16:23:20 -0300

    Adicionando barra de pesquisa por preço

--- store/views.py
+++ store/views.py
@@ -6,19 +6,30 @@ def product_list(request, category_slug=None):
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
 
+    # Filtro por nome (busca)
     query = request.GET.get('q')
     if query:
         products = products.filter(name__icontains=query)
 
+    # Filtro por categoria
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
+
+    # Filtro por preço
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    if min_price:
+        products = products.filter(price__gte=min_price)
+    if max_price:
+        products = products.filter(price__lte=max_price)
 
     return render(request, 'store/product/list.html', {
         'category': category,
         'categories': categories,
         'products': products,
     })
+
 
 
 def product_detail(request, id, slug):
@@ -35,3 +46,4 @@ def sobre(request):
 
 def contato(request):
     return render(request, 'store/contato.html')
+
