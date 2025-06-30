e526fd44a451e2cacff8ee03fee8faa82a21d490
lucas <l_gabriel8@outlook.com>
2025-06-30 14:13:23 -0300

    Criando barra de pesquisa de produtos

--- store/views.py
+++ store/views.py
@@ -5,14 +5,21 @@ def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+
+    query = request.GET.get('q')
+    if query:
+        products = products.filter(name__icontains=query)
+
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
+
     return render(request, 'store/product/list.html', {
         'category': category,
         'categories': categories,
-        'products': products
+        'products': products,
     })
+
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
