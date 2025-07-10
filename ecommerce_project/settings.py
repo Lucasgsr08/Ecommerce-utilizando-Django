e14e420daf49bb43894cdf826fa2198bb3b2f96a
lucas <l_gabriel8@outlook.com>
2025-07-10 18:30:58 -0300

    adicionando token

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -9,9 +9,8 @@ SECRET_KEY = 'django-insecure-@1r)u46*n7z0!^d=^-a8z(16q)y-h$t^2v$#o*@#+@3_#q%8#p
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Liberar tudo para testes locais
 
-# Configurações de aplicativos instalados
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -24,13 +23,12 @@ INSTALLED_APPS = [
     'accounts',
     'orders',
     'crispy_forms',
-    'crispy_bootstrap4',  # Adiciona o template pack do Bootstrap 4
+    'crispy_bootstrap4',
     'rest_framework',
     'rest_framework.authtoken',
-
+    'rest_framework_simplejwt',
 ]
 
-# Configurações de middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -41,10 +39,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# Configurações de URLs
 ROOT_URLCONF = 'ecommerce_project.urls'
 
-# Configurações de templates
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -65,10 +61,8 @@ TEMPLATES = [
     },
 ]
 
-# Configurações de URL
 WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 
-# Configurações do banco de dados
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -76,48 +70,20 @@ DATABASES = {
     }
 }
 
-# Configurações de autenticação
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-
-# Configurações do Django REST Framework
-REST_FRAMEWORK = { 
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ]
-}
-
-
-# Configurações de internacionalização
 LANGUAGE_CODE = 'pt-br'
-
 TIME_ZONE = 'America/Sao_Paulo'
-
 USE_I18N = True
-
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
@@ -126,23 +92,23 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CART_SESSION_ID = 'cart'
 
-# Configurações do django-crispy-forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 CRISPY_ALLOWED_TEMPLATE_PACKS = ['bootstrap4']
+
 LOGIN_URL = 'accounts:login'
 LOGIN_REDIRECT_URL = 'store:product_list'
 LOGOUT_REDIRECT_URL = 'store:product_list'
 
-
-#----------------------------------------
-# Configurações do Django REST Framework
-#----------------------------------------
+# ----------------------------
+# Configurações DRF + JWT
+# ----------------------------
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',  # depois mudamos pra IsAuthenticated onde precisar
+        'rest_framework.permissions.AllowAny',
     ]
 }
--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -4,6 +4,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 
 urlpatterns = [
@@ -13,6 +18,9 @@ urlpatterns = [
     path('orders/', include('orders.urls', namespace='orders')),
     path('', include('store.urls', namespace='store')),
     path('store/', include('store.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
 
 # Serve arquivos de mídia apenas durante o desenvolvimento (DEBUG=True)
